1c938df0789f249558d3b2700ad81912dc4c5e5f
alexandru <boncut.alex10@gmail.com>
2021-04-16 16:03:27 -0500

    removed unnecessary gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,8 @@
 require 'bundler'
 Bundler.require
 
-require_rel '../lib'
+require_relative '../lib/api.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/planet.rb'
+require_relative '../lib/star.rb'
+require_relative '../lib/version.rb'
